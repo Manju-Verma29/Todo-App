5d93463f52296774b7cacfdb6f0110cc43001ffd
Manju Verma <manjuverma2906@gmail.com>
2025-11-19 18:31:08 +0530

    Added Todo component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TodoApp from "./Components/TodoApp"
+
+function App() {
+
+  return (
+    <>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <TodoApp/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
